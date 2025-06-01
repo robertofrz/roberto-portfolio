8d60ca33563d5ddf56eee57872af149ea2112cdf
Roberto Ferraz <robertofrz94@gmail.com>
2025-06-01 14:46:32 -0300

    cv file and projects updated

--- src/data/ProjectsArray.ts
+++ src/data/ProjectsArray.ts
@@ -12,19 +12,19 @@ type projectsType = projectItem[];
 const projects: projectsType = [
   {
     title: "project1.title",
-    screenshot: "/screenshots/code-hire-screenshot.png",
+    screenshot: "/screenshots/solo-dev-screenshot.png",
     description: "project1.description",
-    demo: "https://youtu.be/A43_cVhBIsc",
-    deployed: "https://code-hire-wine.vercel.app/",
-    repository: "https://github.com/robertofrz/code-hire",
+    demo: "https://youtu.be/Thc-Ek7RRWc",
+    deployed: "https://solodev-project.vercel.app",
+    repository: "https://github.com/robertofrz/solo-dev",
   },
   {
     title: "project2.title",
-    screenshot: "/screenshots/auth-project-screenshot.png",
+    screenshot: "/screenshots/code-hire-screenshot.png",
     description: "project2.description",
-    demo: "https://youtu.be/Z50OAzcOU-8",
-    deployed: "https://auth-project-flame-eight.vercel.app/",
-    repository: "https://github.com/robertofrz/auth-project",
+    demo: "https://youtu.be/A43_cVhBIsc",
+    deployed: "https://code-hire-cvbuilder.vercel.app/",
+    repository: "https://github.com/robertofrz/code-hire",
   },
   {
     title: "project3.title",
